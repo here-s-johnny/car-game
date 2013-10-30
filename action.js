beeff7d52f7a35199f0d2c79abd0b1a3cd4515cf
johnny-the-strange <gajl.jan@gmail.com>
2013-10-30 16:45:27 +0100

    message

--- action.js
+++ action.js
@@ -1,29 +1,48 @@
-var context = $('#canvas')[0].getContext("2d");
-var x = 75;
-var y = 75;
-var dx = 2;
-var dy = 4;
+$(function() {
 
-function circle(x, y, radius) {
-  context.beginPath();
-  context.arc(x, y, radius, 0, Math.PI*2, true); 
-  context.closePath();
-  context.fill();
-};
-function draw()
-{
-  context.clearRect(0, 0, $('#canvas')[0].width, $('#canvas')[0].height);
-  circle(x, y, 10);
-  if (x + dx > $('#canvas')[0].width || x + dx < 0)
-    {
-      dx = -dx;
-    }
-  if (y + dy > $('#canvas')[0].height || y + dy < 0 )
-  {
-    dy = -dy;
-  }
-  x += dx;
-  y += dy;
-};
+  ctx = $('#canvas')[0].getContext("2d");
 
-setInterval(draw,10)
+  function drawLine( x_a, y_a, x_b, y_b) {
+    ctx.beginPath();
+    ctx.moveTo(x_a, y_a);
+    ctx.lineTo(x_b, y_b);
+    ctx.stroke();
+  };
+
+ function rectangle( center_x, center_y, sizea, sizeb  ) {
+    ctx.beginPath();
+    ctx.moveTo(center_x - sizea/2, center_y + sizeb/2);
+    ctx.lineTo(center_x + sizea/2, center_y + sizeb/2);
+    ctx.lineTo(center_x + sizea/2, center_y - sizeb/2);
+    ctx.lineTo(center_x - sizea/2, center_y - sizeb/2);
+    ctx.lineTo(center_x - sizea/2, center_y + sizeb/2);
+    ctx.closePath();
+    ctx.fill();
+  };
+
+function rotate( x, y, alpha ) {
+    newx = Math.cos(alpha)*x - Math.sin(alpha)*y;
+    newy = Math.sin(alpha)*x + Math.cos(alpha)*y;
+    return [newx,newy];
+  };
+
+ function drawCar(x_c, y_c, sizea, sizeb, alpha) {
+   e = sizea/2;
+   f = sizeb/2;
+   a = rotate( e, f, alpha);
+   b = rotate(-e, f, alpha);
+   c = rotate(-e,-f, alpha);
+   d = rotate( e,-f, alpha);
+   ctx.beginPath();
+   ctx.moveTo(a[0]+x_c, a[1]+y_c);
+   ctx.lineTo(b[0]+x_c, b[1]+y_c);
+   ctx.lineTo(c[0]+x_c, c[1]+y_c);
+   ctx.lineTo(d[0]+x_c, d[1]+y_c);
+   ctx.lineTo(a[0]+x_c, a[1]+y_c);
+   ctx.closePath();
+   ctx.fill();
+ };
+
+drawCar( 150, 150, 150, 150, 0);
+
+});
