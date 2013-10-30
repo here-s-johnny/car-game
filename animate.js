bc3b233e23bf0eed5c4322ee916e8f92dfe49b28
johnny-the-strange <gajl.jan@gmail.com>
2013-10-30 19:12:25 +0100

    progres 30.10.13

--- animate.js
+++ animate.js
@@ -1,6 +1,8 @@
 $(function() {
 
  var ctx = $('#canvas')[0].getContext("2d");
+ 
+   
 
    function clearCanvas() {
      ctx.save();
@@ -20,6 +22,17 @@ $(function() {
     ctx.fill();
   };
 
+  function frame( center_x, center_y, sizea, sizeb  ) {
+    ctx.beginPath();
+    ctx.moveTo(center_x - sizea/2, center_y + sizeb/2);
+    ctx.lineTo(center_x + sizea/2, center_y + sizeb/2);
+    ctx.lineTo(center_x + sizea/2, center_y - sizeb/2);
+    ctx.lineTo(center_x - sizea/2, center_y - sizeb/2);
+    ctx.lineTo(center_x - sizea/2, center_y + sizeb/2);
+    ctx.closePath();
+    ctx.stroke();
+  };
+
   function rotate( x, y, alpha ) {
     newx = Math.cos(alpha)*x - Math.sin(alpha)*y;
     newy = Math.sin(alpha)*x + Math.cos(alpha)*y;
@@ -29,10 +42,13 @@ $(function() {
  function drawCar(x_c, y_c, sizea, sizeb, alpha) {
    e = sizea/2;
    f = sizeb/2;
+   g = sizea/4;
    a = rotate( e, f, alpha);
    b = rotate(-e, f, alpha);
    c = rotate(-e,-f, alpha);
    d = rotate( e,-f, alpha);
+   h = rotate( g, f, alpha);
+   i = rotate( g,-f, alpha);
    ctx.beginPath();
    ctx.moveTo(a[0]+x_c, a[1]+y_c);
    ctx.lineTo(b[0]+x_c, b[1]+y_c);
@@ -40,13 +56,17 @@ $(function() {
    ctx.lineTo(d[0]+x_c, d[1]+y_c);
    ctx.lineTo(a[0]+x_c, a[1]+y_c);
    ctx.stroke();
+   ctx.beginPath();
+   ctx.moveTo(h[0]+x_c, h[1]+y_c);
+   ctx.lineTo(i[0]+x_c, i[1]+y_c);
+   ctx.stroke();
  };
 
  x_a = 50;
  y_a = 50;
  rot = 0;
  v = 0;
- a = 0;
+ acc = 0;
  drawCar(x_a, y_a, 20, 10, rot);
 
  $(window).keydown(function(event){
@@ -65,21 +85,44 @@ $(function() {
    }
    if(event.keyCode == 38) {
      event.preventDefault();
-     a = 0.1;
-     v = v + a;
-     y_a = y_a + v;
-     clearCanvas();
-     drawCar(x_a, y_a, 20, 10, rot);
+     acc = 0.01;
    }
    if(event.keyCode == 40) {
      event.preventDefault();
-     a = -0.1;
-     v = v + a;
-     y_a = y_a + v;
-     clearCanvas();
-     drawCar(x_a, y_a, 20, 10, rot);
+     acc = -0.01;
    }
  });
+
+
+ $(window).keyup(function(event){
+   if(event.keyCode == 38) {
+     event.preventDefault();
+     acc = 0;
+   }
+
+   if(event.keyCode == 40) {
+     event.preventDefault();
+     acc = 0; 
+   }
+  });
+
+   function resolve() { 
+     if (acc == 0) {
+       v = v/1.05;
+     }
+     else {
+       v = v + acc;
+     } 
+     y_a = y_a + v*Math.sin(rot);
+     x_a = x_a + v*Math.cos(rot);
+     clearCanvas();
+     drawCar(x_a, y_a, 20, 10, rot);
+     frame(500, 500, 1000, 1000);
+ };
+   
+
+   var timer = setInterval(  function(){ resolve()} , 10) ;
+   
 
 });
 
