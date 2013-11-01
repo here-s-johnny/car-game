1ceceba4cc9acf28935f3c3ab0f21dad02f52f20
ghostFaceKillah <garmulewicz@gmail.com>
2013-11-01 22:23:14 +0100

    added some nice throttle handling

--- animate.js
+++ animate.js
@@ -2,70 +2,50 @@ $(function() {
 
   var ctx = $('#canvas')[0].getContext("2d");
   
-
   x_a = 50;
   y_a = 50;
   rot = 0;
   v = 0;
   acc = 0;
-  acc_const = 0.025;
+  acc_const = 0.021;
   decc_const = 1.01;
+  back_speed = 1;
+  handling = 120;
+  braking_force = 4;
   drawCar(ctx, x_a, y_a, 20, 10, rot);
   var pressed = [0,0,0,0];
-
-  $(window).keydown(function(event){
-    if(event.keyCode == 37) {
-      event.preventDefault();
-      pressed[0] = 1;   // left
-    }
-    if(event.keyCode == 39) {
-      event.preventDefault();
-      pressed[3] = 1;   // right
-    }
-    if(event.keyCode == 38) {
-      event.preventDefault();
-      pressed[2] = 1;   // up
-    }
-    if(event.keyCode == 40) {
-      event.preventDefault();
-      pressed[1] = 1;   // down
-    }
-  });
-
-  $(window).keyup(function(event){
-    if(event.keyCode == 37) {
-      event.preventDefault();
-      pressed[0] = 0;   // left
-    }
-    if(event.keyCode == 39) {
-      event.preventDefault();
-      pressed[3] = 0;   // right
-    }
-    if(event.keyCode == 38) {
-      event.preventDefault();
-      pressed[2] = 0;   // up
-    }
-    if(event.keyCode == 40) {
-      event.preventDefault();
-      pressed[1] = 0;   // down
-    }
-  });
+  carStatus = "halt";
 
   function resolve() { 
+    getKeyboardInput(window, pressed);
+
     if ( pressed[2] == 1 && pressed[1] == 0 ) {
-      acc = acc_const;
-      v = v + acc;
-    } else if  ( (pressed[2] == 1 && pressed[1] == 1) || (pressed[2] == 0 && pressed[1] == 0) ) {
-      acc = 0; // nic się nie dzieje - samochód sam zwalnia
+        acc = acc_const;  // accelerate
+        v = v + acc;
+      if (carStatus != "going" && v>0) { carStatus = "going" };
+    } else if ( carStatus == "going"  && ( pressed[2] == 0 && pressed[1] == 1 ) ) {
+        if (v>0) {          // brake
+          a = -braking_force*acc_const;
+          v = v + a;
+        } else { v = 0; acc = 0; }
+      if (v == 0) { carStatus = "justStopped" }
+    } else if  ( carStatus == "justStopped"  &&  ( pressed[2] == 0 && pressed[1] == 0 ) ) {
+      carStatus = "halt";
+    } else if ( ( carStatus == "halt" || carStatus == "goingBack"  )  && ( pressed[2] == 0 && pressed[1] == 1 ) ) {
+        if ( v > -back_speed ) {
+          acc = -0.5*acc_const;  // accelerate backwards
+          v = v + acc;
+        }
+      if (carStatus != "goingBack") { carStatus != "goingBack"; }
+    } else {   
+      acc = 0;           // deccelerate
       v = v/decc_const;
-    } else if  ( pressed[2] == 0 && pressed[1] == 1 ) {
-      acc = -2*acc_const;
-      v = v + acc;
-    }
+    } 
+
     if ( pressed[0] == 1 && pressed[3] == 0 ) {
-      rot = rot - 2*Math.PI/170;
+      rot = rot - 2*Math.PI/handling;
     } else if ( pressed[0] == 0 && pressed[3] == 1 ) {
-      rot = rot + 2*Math.PI/170;
+      rot = rot + 2*Math.PI/handling;
     } 
 
     y_a = y_a + v*Math.sin(rot);
@@ -74,7 +54,7 @@ $(function() {
     drawCar(ctx, x_a, y_a, 20, 10, rot);
     $('#canvas')[0].height/2
     frame(ctx, $('#canvas')[0].width/2, $('#canvas')[0].height/2,
-        $('#canvas')[0].width, $('#canvas')[0].height);
+            $('#canvas')[0].width, $('#canvas')[0].height);
   };
 
   var timer = setInterval(  function(){ resolve()} , 10) ;
--- libanim.js
+++ libanim.js
@@ -55,3 +55,46 @@ function drawCar(ctx, x_c, y_c, sizea, sizeb, alpha) {
   ctx.lineTo(i[0]+x_c, i[1]+y_c);
   ctx.stroke();
 };
+
+function getKeyboardInput(wind, pressed) {
+
+  $(wind).keydown(function(event){
+    if(event.keyCode == 37) {
+      event.preventDefault();
+      pressed[0] = 1;   // left
+    }
+    if(event.keyCode == 39) {
+      event.preventDefault();
+      pressed[3] = 1;   // right
+    }
+    if(event.keyCode == 38) {
+      event.preventDefault();
+      pressed[2] = 1;   // up
+    }
+    if(event.keyCode == 40) {
+      event.preventDefault();
+      pressed[1] = 1;   // down
+    }
+  });
+
+  $(wind).keyup(function(event){
+    if(event.keyCode == 37) {
+      event.preventDefault();
+      pressed[0] = 0;   // left
+    }
+    if(event.keyCode == 39) {
+      event.preventDefault();
+      pressed[3] = 0;   // right
+    }
+    if(event.keyCode == 38) {
+      event.preventDefault();
+      pressed[2] = 0;   // up
+    }
+    if(event.keyCode == 40) {
+      event.preventDefault();
+      pressed[1] = 0;   // down
+    }
+  });
+
+  return pressed;
+};
