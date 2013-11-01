b8dd71e1e31197311e40e73b2308bf9a942638f2
ghostFaceKillah <garmulewicz@gmail.com>
2013-11-01 21:15:09 +0100

    learning code modularization in js

--- animate.js
+++ animate.js
@@ -1,64 +1,7 @@
 $(function() {
 
   var ctx = $('#canvas')[0].getContext("2d");
-
-  function clearCanvas() {
-    ctx.save();
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.restore();
-  };
-
-  function rectangle( center_x, center_y, sizea, sizeb  ) {
-    ctx.beginPath();
-    ctx.moveTo(center_x - sizea/2, center_y + sizeb/2);
-    ctx.lineTo(center_x + sizea/2, center_y + sizeb/2);
-    ctx.lineTo(center_x + sizea/2, center_y - sizeb/2);
-    ctx.lineTo(center_x - sizea/2, center_y - sizeb/2);
-    ctx.lineTo(center_x - sizea/2, center_y + sizeb/2);
-    ctx.closePath();
-    ctx.fill();
-  };
-
-  function frame( center_x, center_y, sizea, sizeb  ) {
-    ctx.beginPath();
-    ctx.moveTo(center_x - sizea/2, center_y + sizeb/2);
-    ctx.lineTo(center_x + sizea/2, center_y + sizeb/2);
-    ctx.lineTo(center_x + sizea/2, center_y - sizeb/2);
-    ctx.lineTo(center_x - sizea/2, center_y - sizeb/2);
-    ctx.lineTo(center_x - sizea/2, center_y + sizeb/2);
-    ctx.closePath();
-    ctx.stroke();
-  };
-
-  function rotate( x, y, alpha ) {
-    newx = Math.cos(alpha)*x - Math.sin(alpha)*y;
-    newy = Math.sin(alpha)*x + Math.cos(alpha)*y;
-    return [newx,newy];
-  };
-
-  function drawCar(x_c, y_c, sizea, sizeb, alpha) {
-    e = sizea/2;
-    f = sizeb/2;
-    g = sizea/4;
-    a = rotate( e, f, alpha);
-    b = rotate(-e, f, alpha);
-    c = rotate(-e,-f, alpha);
-    d = rotate( e,-f, alpha);
-    h = rotate( g, f, alpha);
-    i = rotate( g,-f, alpha);
-    ctx.beginPath();
-    ctx.moveTo(a[0]+x_c, a[1]+y_c);
-    ctx.lineTo(b[0]+x_c, b[1]+y_c);
-    ctx.lineTo(c[0]+x_c, c[1]+y_c);
-    ctx.lineTo(d[0]+x_c, d[1]+y_c);
-    ctx.lineTo(a[0]+x_c, a[1]+y_c);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(h[0]+x_c, h[1]+y_c);
-    ctx.lineTo(i[0]+x_c, i[1]+y_c);
-    ctx.stroke();
-  };
+  
 
   x_a = 50;
   y_a = 50;
@@ -67,7 +10,7 @@ $(function() {
   acc = 0;
   acc_const = 0.025;
   decc_const = 1.01;
-  drawCar(x_a, y_a, 20, 10, rot);
+  drawCar(ctx, x_a, y_a, 20, 10, rot);
   var pressed = [0,0,0,0];
 
   $(window).keydown(function(event){
@@ -127,9 +70,11 @@ $(function() {
 
     y_a = y_a + v*Math.sin(rot);
     x_a = x_a + v*Math.cos(rot);
-    clearCanvas();
-    drawCar(x_a, y_a, 20, 10, rot);
-    frame(500, 500, 1000, 1000);
+    clearCanvas(ctx);
+    drawCar(ctx, x_a, y_a, 20, 10, rot);
+    $('#canvas')[0].height/2
+    frame(ctx, $('#canvas')[0].width/2, $('#canvas')[0].height/2,
+        $('#canvas')[0].width, $('#canvas')[0].height);
   };
 
   var timer = setInterval(  function(){ resolve()} , 10) ;
---
+++ libanim.js
@@ -0,0 +1,57 @@
+function clearCanvas(ctx) {
+  ctx.save();
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.restore();
+};
+
+function rectangle(ctx, center_x, center_y, sizea, sizeb  ) {
+  ctx.beginPath();
+  ctx.moveTo(center_x - sizea/2, center_y + sizeb/2);
+  ctx.lineTo(center_x + sizea/2, center_y + sizeb/2);
+  ctx.lineTo(center_x + sizea/2, center_y - sizeb/2);
+  ctx.lineTo(center_x - sizea/2, center_y - sizeb/2);
+  ctx.lineTo(center_x - sizea/2, center_y + sizeb/2);
+  ctx.closePath();
+  ctx.fill();
+};
+
+function frame(ctx, center_x, center_y, sizea, sizeb  ) {
+  ctx.beginPath();
+  ctx.moveTo(center_x - sizea/2, center_y + sizeb/2);
+  ctx.lineTo(center_x + sizea/2, center_y + sizeb/2);
+  ctx.lineTo(center_x + sizea/2, center_y - sizeb/2);
+  ctx.lineTo(center_x - sizea/2, center_y - sizeb/2);
+  ctx.lineTo(center_x - sizea/2, center_y + sizeb/2);
+  ctx.closePath();
+  ctx.stroke();
+};
+
+function rotate( x, y, alpha ) {
+  newx = Math.cos(alpha)*x - Math.sin(alpha)*y;
+  newy = Math.sin(alpha)*x + Math.cos(alpha)*y;
+  return [newx,newy];
+};
+
+function drawCar(ctx, x_c, y_c, sizea, sizeb, alpha) {
+  e = sizea/2;
+  f = sizeb/2;
+  g = sizea/4;
+  a = rotate( e, f, alpha);
+  b = rotate(-e, f, alpha);
+  c = rotate(-e,-f, alpha);
+  d = rotate( e,-f, alpha);
+  h = rotate( g, f, alpha);
+  i = rotate( g,-f, alpha);
+  ctx.beginPath();
+  ctx.moveTo(a[0]+x_c, a[1]+y_c);
+  ctx.lineTo(b[0]+x_c, b[1]+y_c);
+  ctx.lineTo(c[0]+x_c, c[1]+y_c);
+  ctx.lineTo(d[0]+x_c, d[1]+y_c);
+  ctx.lineTo(a[0]+x_c, a[1]+y_c);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(h[0]+x_c, h[1]+y_c);
+  ctx.lineTo(i[0]+x_c, i[1]+y_c);
+  ctx.stroke();
+};
